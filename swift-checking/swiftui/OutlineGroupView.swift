8878785ce7a387d051eb5f2651e1c227bc1c991c
YutaIke <kei.yt22@gmail.com>
2021-05-07 23:55:06 +0900

    OutlineGroupView作成

---
+++ swift-checking/swiftui/OutlineGroupView.swift
@@ -0,0 +1,20 @@
+//
+//  OutlineGroupView.swift
+//  swift-checking
+//
+//  Created by 池友太 on 2021/05/07.
+//
+
+import SwiftUI
+
+struct OutlineGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OutlineGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        OutlineGroupView()
+    }
+}
